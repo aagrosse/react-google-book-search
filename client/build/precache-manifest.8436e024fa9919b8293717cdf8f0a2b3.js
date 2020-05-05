2632576637878e99a757709a14c6c560d83e02ea
aagrosse <aagrock81@yahoo.com>
2020-05-05 04:32:10 -0400

    heroku build

---
+++ client/build/precache-manifest.8436e024fa9919b8293717cdf8f0a2b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c030026e8929adc6739",
+    "url": "/static/css/main.5ff331b9.chunk.css"
+  },
+  {
+    "revision": "9c030026e8929adc6739",
+    "url": "/static/js/main.5c41e030.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0655bfa5fd5ab0e65035",
+    "url": "/static/js/2.6a12aa79.chunk.js"
+  },
+  {
+    "revision": "70812eed80d7bdc0c774873e503a4093",
+    "url": "/static/media/books.70812eed.png"
+  },
+  {
+    "revision": "ae46a583e1418bd5beefb6dfed4e042c",
+    "url": "/index.html"
+  }
+];
